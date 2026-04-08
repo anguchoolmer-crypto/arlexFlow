a84a7ad39bddc51eccb2d287a90cd3914b48fcf5
ARLEX <anguchoolmer@gmail.com>
2026-04-08 10:10:13 -0600

    chore: configuracion inicial del proyecto Arlex

---
+++ src/app.js
@@ -0,0 +1,13 @@
+export function saludar(nombre){
+return "Hola " + nombre + ", bienvenido a aplicacion  de Arlex";
+}
+export function despedir(nombre){
+return "Hasta pronto, " + nombre + "!arlex";
+}
+export function estadoSistema() {
+return {
+estado: "activo",
+version: "1.0.0",
+message: "sistema calcularalex funcionando correctamente"
+};
+}
---
+++ test/app.test.js
@@ -0,0 +1,28 @@
+import {saludar, despedir, estadoSistema} from "../src/app.js"
+
+function ejecutarPruebas() {
+let pasadas =0;
+let fallidas =0;
+
+const r1 = saludar("Arlex")
+if (r1.includes("Arlex")){
+console.log("Test 1 pasado: funcion saludar en arlex es correcta")
+pasadas++;
+}else{
+console.log("Test 1 fallido: ", r1);
+fallidas++; 
+}
+
+
+const actualEstado = estadoSistema();
+if (actualEstado.estado == "activo"){
+console.log("Test 2: pasado: estadoSistema() funciona en arlex ");
+pasadas++;
+}else{
+console.log("Test 2 fallido: ", estado, "en arlex");
+fallidas++;
+}
+console.log("\nResultados: " + pasadas + " pasadas, ", + fallidas + "fallidas");
+if(fallidas>0) process.exit(1); 
+}
+ejecutarPruebas();
